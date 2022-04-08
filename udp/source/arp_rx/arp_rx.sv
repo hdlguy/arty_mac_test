0d74a75c1f81a49f26eacecebeefa3150166ede7
pete <peter.dudley@precisionplanting.com>
2022-04-08 19:46:40 -0400

    added udp message output to arp_rx.sv, needs more testing with 1:8 input duty factor.

--- udp/source/arp_rx/arp_rx.sv
+++ udp/source/arp_rx/arp_rx.sv
@@ -1,6 +1,7 @@
 // arp_rx.sv - receives raw ethernet frames from the rx mac fifo.
 // If the frame is an arp request, fields are extracted for the arp reply.
 module arp_rx #(
+    parameter logic[47:0] local_mac = 48'h00_0a_35_01_02_03,
     parameter logic[31:0] local_ip  = 32'h10_00_00_80           // 16.0.0.128
 ) (
     input   logic                   clk,
@@ -10,28 +11,49 @@ module arp_rx #(
     input   logic[7:0]              rx_fifo_tdata,
     input   logic                   rx_fifo_tlast, 
     input   logic                   rx_fifo_tuser, 
-    // 
+    // Arp output
     output  logic                   dv_out = 0,
     output  logic[47:0]             remote_mac,
-    output  logic[31:0]             remote_ip
+    output  logic[31:0]             remote_ip,
+    // UDP message output
+    output  logic                   udp_tvalid,
+    input   logic                   udp_tready, // not used
+    output  logic[7:0]              udp_tdata,
+    output  logic                   udp_tlast, 
+    output  logic                   udp_tuser   // not used
 );
 
     localparam int Narp = 42;  // the number of bytes in an Arp packet.
 
     logic[Narp-1:0][7:0] wr_byte = 0; // byte array.
 
+    // rename some bytes to ethernet fields.
+    logic[47:0] dest_mac;
+    assign dest_mac = {wr_byte[0], wr_byte[1], wr_byte[2], wr_byte[3], wr_byte[4], wr_byte[5]};
+    logic[47:0] src_mac;
+    assign src_mac =  {wr_byte[6], wr_byte[7], wr_byte[8], wr_byte[9], wr_byte[10], wr_byte[11]};
+    logic[15:0] frame_type;
+    assign frame_type = {wr_byte[12], wr_byte[13]};
+
     // rename some of the bytes to arp fields.
+    logic[15:0] op_int;
+    assign op_int = {wr_byte[20], wr_byte[21]};
     logic[47:0] remote_mac_int;
     assign remote_mac_int = {wr_byte[22], wr_byte[23], wr_byte[24], wr_byte[25], wr_byte[26], wr_byte[27]};
     logic[31:0] remote_ip_int;
     assign remote_ip_int = {wr_byte[28], wr_byte[29], wr_byte[30], wr_byte[31]};
-    logic[15:0] frame_type_int;
-    assign frame_type_int = {wr_byte[12], wr_byte[13]};
-    logic[15:0] op_int;
-    assign op_int = {wr_byte[20], wr_byte[21]};
     logic[31:0] target_ip_int;
     assign target_ip_int = {wr_byte[38], wr_byte[39], wr_byte[40], wr_byte[41]};
+
+    // rename some of the bytes to IP fields
+    logic[7:0] protocol;
+    assign protocol = wr_byte[23];
+    logic[31:0] dest_ip;
+    assign dest_ip = {wr_byte[30], wr_byte[31], wr_byte[32], wr_byte[33]};
     
+    // rename some of the bytes to UDP fields
+    logic[15:0] dest_port;
+    assign dest_port = {wr_byte[36], wr_byte[37]};
 
     assign rx_fifo_tready = 1; // always ready to receive a byte, no backpressure.
     
@@ -41,6 +63,7 @@ module arp_rx #(
     logic tlast_q = 1, pre_dv_out = 0;
     logic[ 7:0] tdata_q;
     logic[15:0] byte_count;
+    logic udp_found=0;
     always_ff @(posedge clk) begin
     
         dv_in_q <= dv_in;
@@ -63,15 +86,23 @@ module arp_rx #(
             pre_dv_out <= 0;
         end
 
-        if ((pre_dv_out) && (frame_type_int==16'h0806) && (op_int==16'h0001) && (target_ip_int==local_ip)) begin  // if an arp frame
+        if ((pre_dv_out) && (frame_type==16'h0806) && (op_int==16'h0001) && (target_ip_int==local_ip)) begin  // if an arp frame
             remote_mac <= remote_mac_int;
             remote_ip  <= remote_ip_int;
             dv_out <= 1;
         end else begin
             dv_out <= 0;
         end
+
+        if ((byte_count==41) && (dest_mac==local_mac) && (protocol==17) && (dest_ip==local_ip)) udp_found <= 1;
+        if (tlast_q) udp_found <= 0;
         
     end
+    
+    assign udp_tdata = tdata_q;
+    assign udp_tlast = tlast_q;
+    assign udp_tuser = 0;
+    assign udp_tvalid = dv_in_q & udp_found;
     
 endmodule
 
--- udp/source/arp_rx/arp_rx_tb.sv
+++ udp/source/arp_rx/arp_rx_tb.sv
@@ -8,12 +8,18 @@ module arp_rx_tb ();
     logic       dv_out;
     logic[47:0] remote_mac;
     logic[31:0] remote_ip;
+    logic       udp_tvalid;
+    logic       udp_tready;
+    logic[7:0]  udp_tdata;
+    logic       udp_tlast; 
+    logic       udp_tuser;    
 
+    localparam logic[47:0] local_mac = 48'h00_0a_35_01_02_03;
     localparam logic[31:0] local_ip  = 32'h10_00_00_80;  // 16.0.0.128
 
-    logic  clk = 0; localparam  clk_period = 10; always #( clk_period/2)  clk =  ~clk;
+    logic  clk = 0; localparam  clk_period = 10; always #(clk_period/2)  clk =  ~clk;
 
-    arp_rx #(.local_ip(local_ip)) uut (.*);            
+    arp_rx #(.local_mac(local_mac), .local_ip(local_ip)) uut (.*);            
     
     logic fifo_empty, fifo_full;
     mac_fifo rx_fifo (
@@ -25,6 +31,30 @@ module arp_rx_tb ();
 
     // 94:10:3e:b7:e2:01
     
+    // define a legal udp packet
+    localparam int udp_len = 46;
+    logic[0:udp_len-1][7:0] udp_bytes;
+    assign udp_bytes[ 0: 5] = local_mac;
+    assign udp_bytes[ 6:11] = 48'h94_10_3e_b7_e2_01;
+    assign udp_bytes[12:13] = 16'h0800;
+    assign udp_bytes[   14] = 8'h45;
+    assign udp_bytes[   15] = 8'h00;
+    assign udp_bytes[16:17] = 16'h0021;
+    assign udp_bytes[18:19] = 16'h92d8;
+    assign udp_bytes[20:21] = 16'h0400;
+    assign udp_bytes[   22] = 8'h40;
+    assign udp_bytes[   23] = 8'h11;
+    assign udp_bytes[24:25] = 16'h0000;
+    assign udp_bytes[26:29] = 32'h10_00_00_c8;
+    assign udp_bytes[30:33] = 32'h10_00_00_80;
+    assign udp_bytes[34:35] = 16'h9a0d;
+    assign udp_bytes[36:37] = 16'h04d2;
+    assign udp_bytes[38:39] = 16'h000d;
+    assign udp_bytes[40:41] = 16'h0000;
+    assign udp_bytes[42:45] = 32'h55_66_77_88;
+    
+    assign udp_tready = 1;
+    
     initial begin
         fifo_tvalid = 0;
         fifo_tdata  = 8'bxxxx_xxxx;
@@ -34,6 +64,7 @@ module arp_rx_tb ();
 
         forever begin
 
+            // an arp frame
             // destination address for broadcast
             for (int i=0; i<6; i++) begin        
                 fifo_tvalid = 1;
@@ -211,6 +242,22 @@ module arp_rx_tb ();
             #(clk_period*1);
                         
 
+            // a gap
+            fifo_tvalid = 0;
+            fifo_tdata  = 8'bxxxx_xxxx;
+            fifo_tlast = 0;
+            #(clk_period*40);
+            
+            
+            // a udp frame
+            for (int i=0; i<udp_len; i++) begin        
+                fifo_tvalid = 1;
+                fifo_tdata  = udp_bytes[i];
+                if (i==udp_len-1) fifo_tlast = 1; else fifo_tlast = 0;
+                #(clk_period*1);
+            end
+            
+            
             // a gap
             fifo_tvalid = 0;
             fifo_tdata  = 8'bxxxx_xxxx;
