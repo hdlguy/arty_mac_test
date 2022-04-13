4e65a94da3176132d4d3cd6c8e56879cf67b0183
pete <peter.dudley@precisionplanting.com>
2022-04-13 13:13:09 -0400

    improved totlen_tb.sv

--- udp/source/totlen/totlen_tb.sv
+++ udp/source/totlen/totlen_tb.sv
@@ -15,10 +15,7 @@ module totlen_tb ();
 
     logic  clk = 0; localparam  clk_period = 10; always #( clk_period/2)  clk =  ~clk;
 
-    totlen uut (.*);
-    
-    assign m_tready = 1;
-    assign length_tready = 1;
+    totlen uut (.*);    
 
     int length = 4;
     initial begin    
@@ -29,8 +26,7 @@ module totlen_tb ();
         #(clk_period*100);        
                 
         forever begin
-            
-            
+                        
             for (int i=0; i<length; i++) begin
             
                 s_tvalid = 0;
@@ -47,6 +43,49 @@ module totlen_tb ();
             
         end
     end
+    
+    logic[3:0] state=0, next_state;
+    always_comb begin
+    
+        next_state = state;
+        length_tready = 0;
+        m_tready = 0;
+        
+        case (state)
+            0: begin
+                next_state = 1;
+            end
+            
+            1: begin
+                if (length_tvalid) begin
+                    next_state = 2;
+                end
+            end
+            
+            2: begin
+                next_state = 3;
+                length_tready = 1;
+            end
+            
+            3: begin
+                if ((m_tvalid==1) && (m_tlast==1)) begin
+                    next_state = 4; 
+                end
+                m_tready = 1;
+            end
+                
+            4: begin
+                next_state = 0;
+            end
+            
+            default: begin
+                next_state = 0;    
+            end
+            
+        endcase
+    end
+    
+    always_ff @(posedge clk) state <= next_state;
 
 
 endmodule
