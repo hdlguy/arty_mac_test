b8ebad2577bc0541dea3ce2995b5d4bbb9a3148d
pete <peter.dudley@precisionplanting.com>
2022-04-22 08:42:08 -0400

    tweaked spi_program.tcl to work with both configuration flash types.

--- udp/implement/spi_program.tcl
+++ udp/implement/spi_program.tcl
@@ -8,9 +8,9 @@ open_hw_target
 current_hw_device [get_hw_devices]
 refresh_hw_device [current_hw_device]
 
-create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
-#create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {s25fl128sxxxxxx0-spi-x1_x2_x4}] 0]
+create_hw_bitstream -hw_device [get_hw_devices] [get_property PROGRAM.HW_CFGMEM_BITFILE [current_hw_device]];
 
+create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {s25fl128sxxxxxx0-spi-x1_x2_x4}] 0]
 
 set_property PROGRAM.ADDRESS_RANGE  {use_file}              [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
 set_property PROGRAM.FILES [list "./results/top.mcs" ]      [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
@@ -22,11 +22,29 @@ set_property PROGRAM.CFG_PROGRAM    1                       [ get_property PROGR
 set_property PROGRAM.VERIFY         1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
 set_property PROGRAM.CHECKSUM       0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
 
-create_hw_bitstream -hw_device [get_hw_devices] [get_property PROGRAM.HW_CFGMEM_BITFILE [current_hw_device]];
 program_hw_devices  [current_hw_device]; 
 refresh_hw_device   [current_hw_device];
 
-program_hw_cfgmem -hw_cfgmem                                [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+set status [catch {program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]}]
+
+if {$status != 0} {
+    create_hw_cfgmem -hw_device [current_hw_device] -mem_dev [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
+    set_property PROGRAM.ADDRESS_RANGE  {use_file}              [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.FILES [list "./results/top.mcs" ]      [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.PRM_FILE {}                            [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none}     [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.BLANK_CHECK    0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.ERASE          1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.CFG_PROGRAM    1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.VERIFY         1                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+    set_property PROGRAM.CHECKSUM       0                       [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+
+    program_hw_devices  [current_hw_device]; 
+    refresh_hw_device   [current_hw_device];
+
+    program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [current_hw_device]]
+}
+
 boot_hw_device  [current_hw_device]
 close_hw_manager
 
