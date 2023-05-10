a8b1d7212bf98b989b4ed84b4c01fd231547d23e
pedro <pete@hdlguy.com>
2023-05-10 18:39:50 -0400

    creating build area for 100t version of Arty

---
+++ udp/implement_100t/setup.tcl
@@ -0,0 +1,46 @@
+# This script sets up a Vivado project with all ip references resolved.
+# In Linux:
+#   vivado -mode batch -source setup.tcl
+# In windows use xtcl shell
+#   source setup.tcl
+close_project -quiet
+file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
+#
+create_project -force proj 
+set_property part xc7a100ticsg324-1L [current_project]
+set_property target_language verilog [current_project]
+set_property default_lib work [current_project]
+
+read_ip ../source/udp_stack/totlen/totlen_data_fifo/totlen_data_fifo.xci
+read_ip ../source/udp_stack/totlen/totlen_length_fifo/totlen_length_fifo.xci
+read_ip ../source/clk_wiz/clk_wiz.xci
+read_ip ../source/udp_stack/mac_fifo/mac_fifo.xci
+read_ip ../source/udp_stack/udp_fifo/udp_fifo.xci
+read_ip ../source/udp_stack/eth_ila/eth_ila.xci
+reset_target all [get_files *.xci]
+upgrade_ip -quiet  [get_ips *]
+generate_target {all} [get_ips *]
+
+read_verilog -sv ../source/lfsr.v  
+read_verilog -sv ../source/axis_gmii_rx.v  
+read_verilog -sv ../source/axis_gmii_tx.v  
+read_verilog -sv ../source/ssio_sdr_in.v
+read_verilog -sv ../source/eth_mac_1g.v  
+read_verilog -sv ../source/mii_phy_if.v
+read_verilog -sv ../source/eth_mac_mii.v  
+
+read_verilog -sv ../source/udp_frame_rx/udp_frame_rx.sv
+read_verilog -sv ../source/udp_frame_gen/udp_frame_gen.sv
+read_verilog -sv ../source/udp_stack/udp_stack.sv
+read_verilog -sv ../source/udp_stack/totlen/totlen.sv
+read_verilog -sv ../source/temac.sv
+read_verilog -sv ../source/udp_stack/arp_rx/arp_rx.sv
+read_verilog -sv ../source/udp_stack/arp_tx/arp_tx.sv
+
+read_verilog -sv ../source/top.sv
+
+read_xdc ../source/top.xdc  
+
+close_project
+
+
