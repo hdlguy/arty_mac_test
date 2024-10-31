ee4e8d4d777eb27c1a8ff49ca6e5dbc11c780dd7
hdlguy <hdlguy@hdlguy.com>
2024-10-31 09:41:36 -0400

    organizing alex forencich files in separate folder

--- udp/implement/setup.tcl
+++ udp/implement/setup.tcl
@@ -6,8 +6,7 @@
 close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
-create_project -force proj 
-set_property part xc7a35ticsg324-1L [current_project]
+create_project -force -part xc7a100ticsg324-1L proj
 #set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
 set_property target_language verilog [current_project]
 set_property default_lib work [current_project]
