f58f9a79ce250900e748e1bf830ad89ef9c1c726
hdlguy <hdlguy@hdlguy.com>
2024-10-31 10:38:32 -0400

    organizing ethernet stuff

--- udp/implement/setup.tcl
+++ udp/implement/setup.tcl
@@ -6,7 +6,7 @@
 close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
-create_project -force -part xc7a100ticsg324-1L proj
+create_project -force -part xc7a35ticsg324-1L proj
 #set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
 set_property target_language verilog [current_project]
 set_property default_lib work [current_project]
@@ -22,13 +22,13 @@ reset_target all [get_files *.xci]
 upgrade_ip -quiet  [get_ips *]
 generate_target {all} [get_ips *]
 
-read_verilog -sv ../source/lfsr.v  
-read_verilog -sv ../source/axis_gmii_rx.v  
-read_verilog -sv ../source/axis_gmii_tx.v  
-read_verilog -sv ../source/ssio_sdr_in.v
-read_verilog -sv ../source/eth_mac_1g.v  
-read_verilog -sv ../source/mii_phy_if.v
-read_verilog -sv ../source/eth_mac_mii.v  
+read_verilog -sv ../../alex_forencich/lfsr.v  
+read_verilog -sv ../../alex_forencich/axis_gmii_rx.v  
+read_verilog -sv ../../alex_forencich/axis_gmii_tx.v  
+read_verilog -sv ../../alex_forencich/ssio_sdr_in.v
+read_verilog -sv ../../alex_forencich/eth_mac_1g.v  
+read_verilog -sv ../../alex_forencich/mii_phy_if.v
+read_verilog -sv ../../alex_forencich/eth_mac_mii.v  
 
 read_verilog -sv ../source/udp_frame_rx/udp_frame_rx.sv
 read_verilog -sv ../source/udp_frame_gen/udp_frame_gen.sv
